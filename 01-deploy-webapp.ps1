00eb7051024f6ecd1ebd99aaa3d6c2d5556fc3a8
Povel Forsare Källman <64473024+povelfk@users.noreply.github.com>
2024-01-04 13:24:23 +0100

    Delete 01-deploy-webapp.ps1

--- 01-deploy-webapp.ps1
+++
@@ -1,8 +0,0 @@
-az webapp up `
-    --subscription ME-MngEnvMCAP854902-povelf-2 `
-    --resource-group rg-digge2 `
-    --name digge-test-sweden `
-    --plan digge_test_sweden_plan `
-    --location swedencentral `
-    --sku P1V3 `
-    --runtime PYTHON:3.10
